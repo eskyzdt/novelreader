27e3167e0139819d35697f486ed220d46669de1b
timber <eskyzdt@sina.com>
2020-06-11 18:30:45 +0800

    feat(*): V1.1 增加从指定的页数开始阅读的功能.2.增加展示当前阅读的页数功能

--- src/main/java/com/eskyzdt/novelreader/reader/ModuleChoose.java
+++ src/main/java/com/eskyzdt/novelreader/reader/ModuleChoose.java
@@ -1,31 +1,47 @@
 package com.eskyzdt.novelreader.reader;
 
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 public interface ModuleChoose {
+
+    /**
+     * 模板方法
+     * @param fileInputStream
+     */
+    default void read(InputStream fileInputStream) {
+        // 第一步 提示用户选择阅读模式和起始位置
+        Map<String, Object> firstStep = firstStep();
+        String type = (String) firstStep.get("type");
+        String page = (String) firstStep.get("page");
+        int count = (int) firstStep.get("count");
+        // 第二步 提示用户选择指定的页数
+        fileInputStream = startFrom(page, count, fileInputStream);
+        // 字节流转为字符流
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        // 根据入参执行方法,选择阅读模式
+        chooseModule(type, count, inputStreamReader);
+    }
+
+    /**
+     * 选择阅读模式和起始位置
+     * @return
+     */
+    Map<String, Object> firstStep();
+
+    /**
+     * 从指定的页数开始阅读
+     * @param page 页数
+     * @param count 每页多少行
+     * @param inputStream 输入流
+     */
+    InputStream startFrom(String page, int count,InputStream inputStream);
 
     /**
      * 根据入参选择不同的阅读模式
      * @param type 1 自动翻页 2 点击翻页
      */
-    void chooseModule(String type, InputStreamReader inputStreamReader);
+    void chooseModule(String type, int count, InputStreamReader inputStreamReader);
 
-    /**
-     * TODO 从指定的页数开始阅读
-     * @param page
-     */
-    InputStreamReader startFrom(int page, InputStreamReader inputStreamReader);
-
-    /**
-     * 模板方法
-     * @param page
-     * @param type
-     * @param inputStreamReader
-     */
-    default void read(int page, String type, InputStreamReader inputStreamReader) {
-        // 第一步,选择指定的页数
-        inputStreamReader = startFrom(page, inputStreamReader);
-        // 第二步,选择阅读模式
-        chooseModule(type, inputStreamReader);
-    }
 }
--- src/main/java/com/eskyzdt/novelreader/reader/NovelReader.java
+++ src/main/java/com/eskyzdt/novelreader/reader/NovelReader.java
@@ -3,8 +3,6 @@ package com.eskyzdt.novelreader.reader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class NovelReader {
 
@@ -15,22 +13,11 @@ public class NovelReader {
         // 文件路径,在项目的根目录下
         String path = "imnot.txt";
         File file = new File(path);
+        ModuleChoose choose = new ModuleChooseImpl();
         // 输入流读取文件字节流
         FileInputStream fileInputStream = new FileInputStream(file);
-        // 字节流转为字符流
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-        ModuleChoose choose = new ModuleChooseImpl();
-        System.out.println("请选择阅读模式(1. 自动翻页 2.手动翻页):");
-        Scanner scanner = new Scanner(System.in);
-        String type = scanner.nextLine();
-        while (!"1".equals(type) && !"2".equals(type)) {
-            System.out.println("输入错误,请重新选择:");
-            type = scanner.nextLine();
-        }
-        System.out.println("请选择从多少行开始阅读:");
-        int count = scanner.nextInt();
-        // 选择阅读模式并开始阅读
-        choose.read( count ,type, inputStreamReader);
+        // 模板方法
+        choose.read(fileInputStream);
     }
 
 
