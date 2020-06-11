a4ba57ec9bd68dbe579ad14e896cc4912b759992
timber <eskyzdt@sina.com>
2020-06-11 18:30:45 +0800

    feat(*): V1.1 增加从指定的页数开始阅读的功能.2.增加展示当前阅读的页数功能

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# Reference Documentation
+# 说明
 本工具提供文本自动翻页和手动翻页的功能:
 
-# 目前版本V1.0
- ## 支持的功能: 
+# 目前版本V1.1
+ ##支持的功能: 
     # 1.自动翻页
     # 2.手动翻页(按enter键翻页)
     # 3.一次翻页的行数可选
     # 4.自动翻页的秒数可选
     # 5.自动去除文本中的空格,回车,以及换行
+    # 6.从指定的页数开始阅读  ModuleChoose,startFrom()
+    # 7.展示当前阅读的页数
     
-### Guides
+### 指南
  运行程序后,在控制台按提示输入即可
  
     ModuleChoose中的模板read(int page, String type, InputStreamReader inputStreamReader)方法为主逻辑
@@ -17,8 +19,9 @@
 
 
 ### 下一步工作
-    1. 从指定的页数开始阅读  ModuleChoose.startFrom()
-    2. 记录阅读结束时的位置
+    1.提供登陆注册功能
+    2.根据登陆的用户,自动读取上一次阅读的配置
+    
     
 
 
--- src/main/java/com/eskyzdt/novelreader/reader/ModuleChooseImpl.java
+++ src/main/java/com/eskyzdt/novelreader/reader/ModuleChooseImpl.java
@@ -1,58 +1,100 @@
 package com.eskyzdt.novelreader.reader;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-
-;
 
 public class ModuleChooseImpl implements ModuleChoose {
 
     /**
-     * 一次阅读多少行
+     * 一行多少个字
      */
-    static CountDownLatch count;
+    final private static int LINEWORDCOUNT = 60;
+
+    /**
+     * 当前页数
+     */
+    private static long currentPage = 0;
+
+    @Override
+    public Map<String, Object> firstStep() {
+        System.out.println("请选择阅读模式(1.自动翻页 2.手动翻页):");
+        Scanner scanner = new Scanner(System.in);
+        String type = scanner.nextLine();
+        while (!"1".equals(type) && !"2".equals(type)) {
+            System.out.println("输入错误,请重新选择:");
+            type = scanner.nextLine();
+        }
+        System.out.println("请选择从多少页开始阅读:");
+        String page = scanner.nextLine();
+        System.out.println("请输入每页的行数:");
+        int count = scanner.nextInt();
+
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("type", type);
+        result.put("page", page);
+        result.put("count", count);
+        return result;
+    }
+
+    @Override
+    public InputStream startFrom(String page, int column, InputStream inputStream) {
+        try {
+            Long pageLong = Long.valueOf(page);
+            // 把当前页数置为跳过的页数+1
+            currentPage = pageLong + 1;
+            // LINEWORDCOUNT是一行多少个字符,而一个字符占两个字节,所以要乘以2
+            // 再乘以column,从第多少页开始
+            long byteSize = pageLong * LINEWORDCOUNT * 2 * column;
+            inputStream.skip(byteSize);
+        } catch (Exception e) {
+            System.out.println("请重新选择从多少页开始阅读");
+            Scanner scanner = new Scanner(System.in);
+            page = scanner.nextLine();
+            startFrom(page, column, inputStream);
+        }
+        return inputStream;
+    }
 
     /**
      * 根据入参选择不同的阅读模式
+     *
      * @param type 1 自动翻页 2 点击翻页
      */
     @Override
-    public void chooseModule(String type, InputStreamReader inputStreamReader) {
+    public void chooseModule(String type, int count, InputStreamReader inputStreamReader) {
         switch (type) {
-            case "1" :
-                auto(inputStreamReader);
+            case "1":
+                auto(inputStreamReader, count);
                 break;
-            case "2" :
-                hand(inputStreamReader);
+            case "2":
+                hand(inputStreamReader, count);
                 break;
             default:
                 break;
         }
     }
 
-    @Override
-    public InputStreamReader startFrom(int page, InputStreamReader inputStreamReader) {
-        return inputStreamReader;
-    }
-
     /**
      * 自动翻页的方法
-     * @param inputStreamReader
+     *
+     * @param inputStreamReader 字节流到字符流的桥接器
      */
-    private void auto(InputStreamReader inputStreamReader) {
+    private void auto(InputStreamReader inputStreamReader, int count) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入一次翻页的行数:");
-        int count = scanner.nextInt();
         System.out.println("请输入多少秒翻页一次:");
         int seconds = scanner.nextInt();
         while (true) {
             // 输入流
-            int i = 0;
+            int i;
             try {
                 i = readLine(count, inputStreamReader);
+                // 自动读取时不需要pressEnter,所以要加个空行
+                System.out.println("                                          " + (currentPage++));
                 TimeUnit.SECONDS.sleep(seconds);
             } catch (Exception e) {
                 // todo 记录阅读位置
@@ -67,12 +109,10 @@ public class ModuleChooseImpl implements ModuleChoose {
 
     /**
      * 手动翻页的方法
+     *
      * @param inputStreamReader
      */
-    private void hand(InputStreamReader inputStreamReader) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入一次翻页的行数:");
-        int count = scanner.nextInt();
+    private void hand(InputStreamReader inputStreamReader, int count) {
         while (true) {
             // 输入流
             int result;
@@ -91,31 +131,40 @@ public class ModuleChooseImpl implements ModuleChoose {
     }
 
     /**
-     *
-     * @param n 一次读取的行数
+     * @param n                 一次读取的行数
      * @param inputStreamReader 文件的输入流
      * @return
      * @throws IOException
      */
     private static int readLine(int n, InputStreamReader inputStreamReader) throws IOException {
-        count = new CountDownLatch(n);
-        int read = -1;
-        while (count.getCount() != 0) {
-            char[] chars = new char[60];
-            read = inputStreamReader.read(chars);
-            for (int i = 0; i < chars.length; i++) {
-                char aChar = chars[i];
-                // 去年空格和换行符之类的
-                if (aChar != '\r' & aChar != '\n' & aChar != ' ') {
-                    System.out.print(chars[i]);
+        int result;
+        // 读取字符,每次读取这么多,当然去除掉空格什么的达不到要求的行数
+        // 但是就这样,设计如此
+        int lengthPerRead = n * LINEWORDCOUNT;
+        char[] readChar = new char[lengthPerRead];
+        // 读取字符,如果返回值为-1说明读完了
+        result = inputStreamReader.read(readChar);
+        // 对读取到的字符进行处理
+        // 创建一个临时的字节
+        char[] charTem = new char[LINEWORDCOUNT];
+        int countTem = 0;
+        for (int i = 0; i < lengthPerRead; i++) {
+            char aChar = readChar[i];
+            // 去年空格和换行符之类的
+            if (aChar != '\r' & aChar != '\n' & aChar != ' ') {
+                try {
+                    charTem[countTem] = aChar;
+                } catch (Exception e) {
+                    // 存满了,打印
+                    System.out.println(charTem);
+                    // 初始化
+                    charTem = new char[LINEWORDCOUNT];
+                    countTem = 0;
                 }
+                countTem++;
             }
-            // 打印换行
-            System.out.println();
-            // 每执行一次锁减一
-            count.countDown();
         }
-        return read;
+        return result;
     }
 
     private static void pressEnter() {
